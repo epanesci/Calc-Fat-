c5ac3edd095a4d7c12dd3e27ab6572e64203c4d3
Emma <epanesci@gmail.com>
2020-08-19 18:45:23 -0300

    se agrego cadera al seleccionar mujer

--- js/app.js
+++ js/app.js
@@ -1,45 +1,56 @@
 
-let listadoDePesos = '';
-for (i = 30; i<200 ; i++){
-    listadoDePesos += (i === 90) ? `<option selected = "true" value="${i}">${i}</option>` : `<option value="${i}">${i}</option>`;
+const cargaInicial = () => {
+    let listadoDePesos = '';
+    for (i = 30; i<200 ; i++){
+        listadoDePesos += (i === 90) ? `<option selected = "true" value="${i}">${i}</option>` : `<option value="${i}">${i}</option>`;
+    }
+    let listadoDeAlturas = '';
+    for (i = 140; i<240 ; i++){
+    listadoDeAlturas += (i===190) ? `<option  selected = "true" value="${i}">${i}</option>`:`<option value="${i}">${i}</option>`;
+    }
+    let listadoDeCinturas = '';
+    for (i = 50; i<200 ; i++){
+    listadoDeCinturas += (i===90) ?  `<option selected = "true" value="${i}">${i}</option>` : `<option value="${i}">${i}</option>`;
+    }
+    let listadoDeCaderas = '';
+    for (i = 50; i<200 ; i++){
+    listadoDeCaderas += (i===90) ?  `<option selected = "true" value="${i}">${i}</option>` : `<option value="${i}">${i}</option>`;
+    }
+    let listadoDeCuellos = '';
+    for (i = 25; i<60 ; i++){
+    listadoDeCuellos += (i===45) ? `<option selected = "true" value="${i}">${i}</option>`: `<option value="${i}">${i}</option>`;
+    }
+    let listadoDeEdades = '';
+    for (i = 18; i<90 ; i++){
+    listadoDeEdades += (i===45) ? `<option selected = "true" value="${i}">${i}</option>`: `<option value="${i}">${i}</option>`;
+    }
+    document.getElementById('peso').innerHTML = listadoDePesos;
+    document.getElementById('altura').innerHTML = listadoDeAlturas;
+    document.getElementById('cintura').innerHTML = listadoDeCinturas;
+    document.getElementById('cadera').innerHTML = listadoDeCaderas;
+    document.getElementById('cuello').innerHTML = listadoDeCuellos;
+    document.getElementById('edad').innerHTML = listadoDeEdades;
 }
-let listadoDeAlturas = '';
-for (i = 140; i<240 ; i++){
-   listadoDeAlturas += (i===190) ? `<option  selected = "true" value="${i}">${i}</option>`:`<option value="${i}">${i}</option>`;
-}
-let listadoDeCinturas = '';
-for (i = 50; i<200 ; i++){
-   listadoDeCinturas += (i===90) ?  `<option selected = "true" value="${i}">${i}</option>` : `<option value="${i}">${i}</option>`;
-}
-let listadoDeCuellos = '';
-for (i = 25; i<60 ; i++){
-   listadoDeCuellos += (i===45) ? `<option selected = "true" value="${i}">${i}</option>`: `<option value="${i}">${i}</option>`;
-}
-let listadoDeEdades = '';
-for (i = 18; i<90 ; i++){
-   listadoDeEdades += (i===45) ? `<option selected = "true" value="${i}">${i}</option>`: `<option value="${i}">${i}</option>`;
-}
-document.getElementById('peso').innerHTML = listadoDePesos;
-document.getElementById('altura').innerHTML = listadoDeAlturas;
-document.getElementById('cintura').innerHTML = listadoDeCinturas;
-document.getElementById('cuello').innerHTML = listadoDeCuellos;
-document.getElementById('edad').innerHTML = listadoDeEdades;
 
 
-
-
-
-
+cargaInicial();
+let peso = document.getElementById('peso').value;
+let altura = document.getElementById('altura').value;
+let cintura = document.getElementById('cintura').value;
+let cadera = document.getElementById('cadera').value;
+let cuello = document.getElementById('cuello').value;
+let sexoSelector = document.getElementById('sexo');
+let caderaSelector = document.querySelector('.caderaBox');
+sexo.addEventListener('click', () => {
+  
+    caderaSelector.style.display = (sexoSelector.value === 'hombre') ? 'none' :'block';
+    
+});
 document.getElementById('calcular').addEventListener('click', () => {
-    let peso = document.getElementById('peso').value;
-    let altura = document.getElementById('altura').value;
-    let cintura = document.getElementById('cintura').value;
-    let cuello = document.getElementById('cuello').value;
-
-
-
+    
+    
     let edad = 32; 
-    let sexo = 1; 
+  
     let porcGrasa = Math.floor((495/(1.0324-0.19077*(Math.log10(cintura-cuello))+0.15456*(Math.log10(altura)))-450)*100)/100;
     
     let kgGrasa = peso * porcGrasa /100;
@@ -52,4 +63,3 @@ document.getElementById('calcular').addEventListener('click', () => {
 
 
 
-// 86.010 * 1,75587 - 70.041 * 2,2764 + 36,76
